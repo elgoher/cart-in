0df21efe512a3a3156c40207b719d43da1e97a63
Eliu Gonzalez Hernandez <elgoher@gmail.com>
2014-07-25 10:12:21 -0500

    Inicio de desarrollo solo carpertas donde estaran los archivos

---
+++ js/cart.js
@@ -0,0 +1,18 @@
+
+
+function AddToCart() {
+    console.log("agregando");
+
+}
+
+function buy(argument) {
+
+}
+
+function ListArticles(argument) {
+
+}
+
+$(document).on("ready", function(event) {
+    console.log("js cargado");
+});
